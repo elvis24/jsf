633268bc17a032c886c22017a778e40298265172
Elvis <elvis@MacBook-Pro-de-Elvis.local>
2019-08-26 17:20:06 -0500

    inicial

---
+++ src/main/java/pe/isil/demo01/Customer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.isil.demo01;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+@Named
+@RequestScoped
+public class Customer {
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+    
+}
